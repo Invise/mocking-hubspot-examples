340f43a107f588a7f9c61ad521588899d49bcb4c
Anders Jürisoo <anders.jurisoo@invise.se>
2022-10-14 12:47:53 +0200

    Add list of tests

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # How to mock Hubspot API?
 
+mock-basic-api.test.ts :white_check_mark:  
+mock-get-all.test.ts :white_check_mark:  
+multiple-mocks.test.ts :error:  
+
 ## Tech stack
 * NodeJS
 * TS
--- __test__/mock-get-all.test.ts
+++ __test__/mock-get-all.test.ts
@@ -1,18 +1,4 @@
 import { Client } from "@hubspot/api-client";
-const mockCreate = jest.fn()
-const mockUpdate = jest.fn()
-
-jest.mock("@hubspot/api-client/lib/codegen/crm/products/types/PromiseAPI", () => {
-    return {
-        ...jest.requireActual("@hubspot/api-client/lib/codegen/crm/products/types/PromiseAPI"),
-        PromiseBasicApi: jest.fn().mockImplementation(() => {
-            return {
-                create: mockCreate,
-                update: mockUpdate,
-            }
-        })
-    }
-})
 
 jest.mock("@hubspot/api-client/lib/src/discovery/crm/products/ProductsDiscovery", () => {
     return {
@@ -34,19 +20,9 @@ jest.mock("@hubspot/api-client/lib/src/discovery/crm/products/ProductsDiscovery"
     }
 });
 
-it('can mock basicApi', async () => {
-    const client = new Client({accessToken: 'dummy'});
-    const product = {properties: {name: 'non-existing-product-name'}};
-
-    await client.crm.products.basicApi.create(product);
-
-    expect(mockUpdate).not.toHaveBeenCalled();
-    expect(mockCreate).toHaveBeenCalledWith(product);
-})
-
 it('can mock getAll', async () => {
     const client = new Client({accessToken: 'dummy'});
     const results = await client.crm.products.getAll()
 
-    expect([ { id: 1, properties: { name: 'existing-product-name' } } ]).not.toHaveBeenCalled();
+    expect(results).toStrictEqual([ { id: 1, properties: { name: 'existing-product-name' } } ]);
 })
